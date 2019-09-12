295402a5aad8156f395e4aa5673852929a797986
Brian Simonton <bsimonton78@gmail.com>
2019-09-12 06:04:05 +0000

    Automatically backed up by Learn

--- app/models/validator.rb
+++ app/models/validator.rb
@@ -1,7 +1,18 @@
 class Validator < ActiveModel::Validator 
   
 
-
+def validate(record)
+  
+  
+  if record.title != nil
+          
+        unless record.title.match?(/(You|Won't Believe|Secret|Top \d|Guess)/)
+            
+            
+      record.errors[:title] << 'Need a clickbait title please!'
+            end
+        end
+    end
 
 
 
